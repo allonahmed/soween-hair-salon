9f6ef5a7beb0463cf906fc1ad1af99c33ddb8042
allonahmed <allonahmed@mec.science>
2022-07-27 16:38:53 -0400

    file clean and removed unneccesary content

--- src/pages/Renovations.js
+++ src/pages/Renovations.js
@@ -2,23 +2,23 @@ import React, { useState } from "react";
 import ReactPlayer from "react-player";
 import "../styles/renovations/renovations.css";
 import video from "../media/videos/1080p.mov";
-import thumb from "../media/photos/videothumbnail.png";
+
+const playIcon = (
+  <div className="play-wrapper">
+    <i className="play-icon fa fa-play" />
+  </div>
+);
 
 const Renovations = () => {
-  const playIcon = (
-    <div className="play-wrapper">
-      <i className="play-icon fa fa-play" />
-    </div>
-  );
-  // const playIcon = <FontAwesomeIcon icon={faPlay} className="play-icon" />;
-  const thumbnail = <img className="thumb" src={thumb} alt="thumbnail" />;
+
   const [style, setStyle] = useState({ display: "block" });
+
   return (
     <div className="renovations" id="renovations">
       <div className="inovations-info">
         <h4>Renovations</h4>
         <h3>
-          Located in the heart of BedStuy, recently underwent renovations to
+          Located in the heart of BedStuy, we recently underwent renovations to
           improve the quality of the Soween experience. The modern and sleek new
           look has made it a must visit!
         </h3>
@@ -27,6 +27,7 @@ const Renovations = () => {
           <a
             href="https://go.booker.com/location/SoweenHairSalon/service-menu"
             target="_blank"
+            rel='noreferrer'
           >
             Book Your Visit{" "}
           </a>
@@ -47,6 +48,7 @@ const Renovations = () => {
             stopOnUnmount={false}
             width="100%"
             height="100%"
+            style={style}
           />
         </div>
       </div>
